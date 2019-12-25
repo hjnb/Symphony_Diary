5a4534ab0a57a2f0ef048c73ffced0420c2be78e
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2019-12-25 15:34:50 +0900

    勤務画面少し作成。

--- Symphony_Diary/Symphony_Diary/勤務画面.Designer.vb
+++ Symphony_Diary/Symphony_Diary/勤務画面.Designer.vb
@@ -22,16 +22,141 @@ Partial Class 勤務画面
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.adBox = New ADBox.adBox()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnRowInsert = New System.Windows.Forms.Button()
+        Me.btnRowDelete = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.dgvWork = New Symphony_Diary.WorkDataGridView(Me.components)
+        CType(Me.dgvWork, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'adBox
+        '
+        Me.adBox.dateText = "25"
+        Me.adBox.Location = New System.Drawing.Point(30, 25)
+        Me.adBox.Mode = 3
+        Me.adBox.monthText = "12"
+        Me.adBox.Name = "adBox"
+        Me.adBox.Size = New System.Drawing.Size(130, 35)
+        Me.adBox.TabIndex = 0
+        Me.adBox.yearText = "2019"
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(168, 30)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(50, 25)
+        Me.btnDisplay.TabIndex = 1
+        Me.btnDisplay.Text = "表示"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnRowInsert
+        '
+        Me.btnRowInsert.Location = New System.Drawing.Point(393, 30)
+        Me.btnRowInsert.Name = "btnRowInsert"
+        Me.btnRowInsert.Size = New System.Drawing.Size(50, 25)
+        Me.btnRowInsert.TabIndex = 3
+        Me.btnRowInsert.Text = "行挿入"
+        Me.btnRowInsert.UseVisualStyleBackColor = True
+        '
+        'btnRowDelete
+        '
+        Me.btnRowDelete.Location = New System.Drawing.Point(444, 30)
+        Me.btnRowDelete.Name = "btnRowDelete"
+        Me.btnRowDelete.Size = New System.Drawing.Size(50, 25)
+        Me.btnRowDelete.TabIndex = 4
+        Me.btnRowDelete.Text = "行削除"
+        Me.btnRowDelete.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(623, 26)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(69, 33)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "登録"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(691, 26)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(69, 33)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "換算"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(827, 26)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(69, 33)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "印刷"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(759, 26)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(69, 33)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "削除"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(895, 26)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(69, 33)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "個人別"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'dgvWork
+        '
+        Me.dgvWork.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvWork.Location = New System.Drawing.Point(30, 68)
+        Me.dgvWork.Name = "dgvWork"
+        Me.dgvWork.RowTemplate.Height = 21
+        Me.dgvWork.Size = New System.Drawing.Size(933, 582)
+        Me.dgvWork.TabIndex = 2
         '
         '勤務画面
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(999, 694)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnRowDelete)
+        Me.Controls.Add(Me.btnRowInsert)
+        Me.Controls.Add(Me.dgvWork)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.adBox)
         Me.Name = "勤務画面"
         Me.Text = "Diary "
+        CType(Me.dgvWork, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents adBox As ADBox.adBox
+    Friend WithEvents btnDisplay As System.Windows.Forms.Button
+    Friend WithEvents dgvWork As Symphony_Diary.WorkDataGridView
+    Friend WithEvents btnRowInsert As System.Windows.Forms.Button
+    Friend WithEvents btnRowDelete As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
 End Class
--- Symphony_Diary/Symphony_Diary/勤務画面.vb
+++ Symphony_Diary/Symphony_Diary/勤務画面.vb
@@ -3,6 +3,15 @@
     'フォームタイプ
     Private formType As String
 
+    '入力可能行数（勤務入力部分）
+    Private Const INPUT_ROW_COUNT As Integer = 100
+
+    '勤務割データテーブル
+    Private workDt As DataTable
+
+    '曜日配列
+    Private dayCharArray() As String = {"日", "月", "火", "水", "木", "金", "土"}
+
     ''' <summary>
     ''' コンストラクタ
     ''' </summary>
@@ -14,7 +23,120 @@
         Me.Text = "Diary " & formType & " 勤務表"
     End Sub
 
+    ''' <summary>
+    ''' loadイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
     Private Sub 勤務画面_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'データグリッドビュー初期設定
+        initDgvWork()
+    End Sub
 
+    ''' <summary>
+    ''' データグリッドビュー初期設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initDgvWork()
+        Util.EnableDoubleBuffering(dgvWork)
+        With dgvWork
+            .AllowUserToAddRows = False '行追加禁止
+            .AllowUserToResizeColumns = False '列の幅をユーザーが変更できないようにする
+            .AllowUserToResizeRows = False '行の高さをユーザーが変更できないようにする
+            .AllowUserToDeleteRows = False '行削除禁止
+            .RowHeadersVisible = False '行ヘッダー非表示
+            .SelectionMode = DataGridViewSelectionMode.CellSelect
+            .RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing
+            .ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+            .BackgroundColor = Color.FromKnownColor(KnownColor.Control)
+            .DefaultCellStyle.SelectionForeColor = Color.Black
+            .RowTemplate.Height = 16
+            .ColumnHeadersHeight = 19
+            .ShowCellToolTips = False
+            .EnableHeadersVisualStyles = False
+            .DefaultCellStyle.Font = New Font("MS UI Gothic", 8.5)
+        End With
+
+        dgvWork.Columns.Clear()
+        workDt = New DataTable()
+
+        '列定義
+        workDt.Columns.Add("Seq", GetType(String))
+        workDt.Columns.Add("Ym", GetType(String))
+        workDt.Columns.Add("Kei", GetType(String))
+        workDt.Columns.Add("Syu", GetType(String))
+        workDt.Columns.Add("Nam", GetType(String))
+        workDt.Columns.Add("Type", GetType(String))
+        For i As Integer = 1 To 31
+            workDt.Columns.Add("Y" & i, GetType(String))
+        Next
+
+        '空行追加
+        For i = 0 To INPUT_ROW_COUNT
+            workDt.Rows.Add(workDt.NewRow())
+        Next
+
+        '表示
+        dgvWork.DataSource = workDt
+
+        '幅設定等
+        With dgvWork
+            '非表示列
+            .Columns("Seq").Visible = False
+            .Columns("Ym").Visible = False
+
+            '行固定
+            .Rows(0).Frozen = True
+
+            '列固定
+            .Columns("Type").Frozen = True
+
+            '並び替え禁止
+            For Each c As DataGridViewColumn In .Columns
+                c.SortMode = DataGridViewColumnSortMode.NotSortable
+            Next
+
+            '形態
+            With .Columns("Kei")
+                .Width = 90
+                .HeaderText = "形態"
+                .HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            End With
+
+            '種別列
+            With .Columns("Syu")
+                .Width = 140
+                .HeaderText = "職種"
+                .HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            End With
+
+            '氏名列
+            With .Columns("Nam")
+                .Width = 92
+                .HeaderText = "氏名"
+                .HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+            End With
+
+            '予定or変更列
+            With .Columns("Type")
+                .Width = 32
+                .HeaderText = ""
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            End With
+
+            'Y1～Y31の列
+            For i As Integer = 1 To 31
+                With .Columns("Y" & i)
+                    .Width = 43
+                    .HeaderText = i.ToString()
+                    .HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter
+                    .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                End With
+            Next
+        End With
     End Sub
 End Class
