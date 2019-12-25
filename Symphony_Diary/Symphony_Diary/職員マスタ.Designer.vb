7b0424ae6a7edf82eb6f24deb9ef04d093f72bee
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2019-12-25 17:05:38 +0900

    職員マスタ：表示まで作成かな

--- Symphony_Diary/Symphony_Diary/職員マスタ.Designer.vb
+++ Symphony_Diary/Symphony_Diary/職員マスタ.Designer.vb
@@ -22,8 +22,394 @@ Partial Class 職員マスタ
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.syuListBox = New System.Windows.Forms.ListBox()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnRegist = New System.Windows.Forms.Button()
+        Me.memoBox = New System.Windows.Forms.TextBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton9 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton10 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton11 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton12 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton7 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton8 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.syuLabel = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.namBox = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.idBox = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dgvNamM = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.dgvNamM, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.syuListBox)
+        Me.GroupBox1.Controls.Add(Me.btnDelete)
+        Me.GroupBox1.Controls.Add(Me.btnRegist)
+        Me.GroupBox1.Controls.Add(Me.memoBox)
+        Me.GroupBox1.Controls.Add(Me.GroupBox3)
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.syuLabel)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.namBox)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.idBox)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(19, 15)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(775, 238)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'syuListBox
+        '
+        Me.syuListBox.BackColor = System.Drawing.SystemColors.Control
+        Me.syuListBox.FormattingEnabled = True
+        Me.syuListBox.ItemHeight = 12
+        Me.syuListBox.Location = New System.Drawing.Point(603, 30)
+        Me.syuListBox.Name = "syuListBox"
+        Me.syuListBox.Size = New System.Drawing.Size(144, 196)
+        Me.syuListBox.TabIndex = 15
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(492, 199)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 33)
+        Me.btnDelete.TabIndex = 14
+        Me.btnDelete.Text = "削除"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnRegist
+        '
+        Me.btnRegist.Location = New System.Drawing.Point(415, 199)
+        Me.btnRegist.Name = "btnRegist"
+        Me.btnRegist.Size = New System.Drawing.Size(75, 33)
+        Me.btnRegist.TabIndex = 13
+        Me.btnRegist.Text = "登録"
+        Me.btnRegist.UseVisualStyleBackColor = True
+        '
+        'memoBox
+        '
+        Me.memoBox.ImeMode = System.Windows.Forms.ImeMode.Hiragana
+        Me.memoBox.Location = New System.Drawing.Point(87, 207)
+        Me.memoBox.Name = "memoBox"
+        Me.memoBox.Size = New System.Drawing.Size(288, 19)
+        Me.memoBox.TabIndex = 12
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.RadioButton9)
+        Me.GroupBox3.Controls.Add(Me.RadioButton10)
+        Me.GroupBox3.Controls.Add(Me.RadioButton11)
+        Me.GroupBox3.Controls.Add(Me.RadioButton12)
+        Me.GroupBox3.Controls.Add(Me.RadioButton5)
+        Me.GroupBox3.Controls.Add(Me.RadioButton6)
+        Me.GroupBox3.Controls.Add(Me.RadioButton7)
+        Me.GroupBox3.Controls.Add(Me.RadioButton8)
+        Me.GroupBox3.Location = New System.Drawing.Point(87, 113)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(480, 82)
+        Me.GroupBox3.TabIndex = 11
+        Me.GroupBox3.TabStop = False
+        '
+        'RadioButton9
+        '
+        Me.RadioButton9.AutoSize = True
+        Me.RadioButton9.Location = New System.Drawing.Point(328, 16)
+        Me.RadioButton9.Name = "RadioButton9"
+        Me.RadioButton9.Size = New System.Drawing.Size(99, 16)
+        Me.RadioButton9.TabIndex = 11
+        Me.RadioButton9.TabStop = True
+        Me.RadioButton9.Text = "ﾍﾙﾊﾟｰｽﾃｰｼｮﾝ"
+        Me.RadioButton9.UseVisualStyleBackColor = True
+        '
+        'RadioButton10
+        '
+        Me.RadioButton10.AutoSize = True
+        Me.RadioButton10.Location = New System.Drawing.Point(328, 50)
+        Me.RadioButton10.Name = "RadioButton10"
+        Me.RadioButton10.Size = New System.Drawing.Size(95, 16)
+        Me.RadioButton10.TabIndex = 10
+        Me.RadioButton10.TabStop = True
+        Me.RadioButton10.Text = "生活支援ﾊｳｽ"
+        Me.RadioButton10.UseVisualStyleBackColor = True
+        '
+        'RadioButton11
+        '
+        Me.RadioButton11.AutoSize = True
+        Me.RadioButton11.Location = New System.Drawing.Point(165, 50)
+        Me.RadioButton11.Name = "RadioButton11"
+        Me.RadioButton11.Size = New System.Drawing.Size(124, 16)
+        Me.RadioButton11.TabIndex = 9
+        Me.RadioButton11.TabStop = True
+        Me.RadioButton11.Text = "老人介護支援ｾﾝﾀｰ"
+        Me.RadioButton11.UseVisualStyleBackColor = True
+        '
+        'RadioButton12
+        '
+        Me.RadioButton12.AutoSize = True
+        Me.RadioButton12.Location = New System.Drawing.Point(16, 50)
+        Me.RadioButton12.Name = "RadioButton12"
+        Me.RadioButton12.Size = New System.Drawing.Size(95, 16)
+        Me.RadioButton12.TabIndex = 8
+        Me.RadioButton12.TabStop = True
+        Me.RadioButton12.Text = "居宅介護支援"
+        Me.RadioButton12.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(245, 16)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(73, 16)
+        Me.RadioButton5.TabIndex = 7
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "ﾃﾞｲｻｰﾋﾞｽ"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Location = New System.Drawing.Point(165, 16)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(71, 16)
+        Me.RadioButton6.TabIndex = 6
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "ｼｮｰﾄｽﾃｲ"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'RadioButton7
+        '
+        Me.RadioButton7.AutoSize = True
+        Me.RadioButton7.Location = New System.Drawing.Point(91, 16)
+        Me.RadioButton7.Name = "RadioButton7"
+        Me.RadioButton7.Size = New System.Drawing.Size(47, 16)
+        Me.RadioButton7.TabIndex = 5
+        Me.RadioButton7.TabStop = True
+        Me.RadioButton7.Text = "事務"
+        Me.RadioButton7.UseVisualStyleBackColor = True
+        '
+        'RadioButton8
+        '
+        Me.RadioButton8.AutoSize = True
+        Me.RadioButton8.Location = New System.Drawing.Point(16, 16)
+        Me.RadioButton8.Name = "RadioButton8"
+        Me.RadioButton8.Size = New System.Drawing.Size(47, 16)
+        Me.RadioButton8.TabIndex = 4
+        Me.RadioButton8.TabStop = True
+        Me.RadioButton8.Text = "特養"
+        Me.RadioButton8.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Controls.Add(Me.RadioButton2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton1)
+        Me.GroupBox2.Location = New System.Drawing.Point(87, 66)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(480, 40)
+        Me.GroupBox2.TabIndex = 10
+        Me.GroupBox2.TabStop = False
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(328, 14)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(95, 16)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "常勤以外兼務"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(211, 14)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(95, 16)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "常勤以外専従"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(114, 14)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(71, 16)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "常勤兼務"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(16, 14)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(71, 16)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "常勤専従"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'syuLabel
+        '
+        Me.syuLabel.AutoSize = True
+        Me.syuLabel.Location = New System.Drawing.Point(442, 36)
+        Me.syuLabel.Name = "syuLabel"
+        Me.syuLabel.Size = New System.Drawing.Size(0, 12)
+        Me.syuLabel.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(396, 36)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(29, 12)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "職種"
+        '
+        'namBox
+        '
+        Me.namBox.ImeMode = System.Windows.Forms.ImeMode.Hiragana
+        Me.namBox.Location = New System.Drawing.Point(214, 33)
+        Me.namBox.Name = "namBox"
+        Me.namBox.Size = New System.Drawing.Size(97, 19)
+        Me.namBox.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(176, 36)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(29, 12)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "氏名"
+        '
+        'idBox
+        '
+        Me.idBox.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.idBox.Location = New System.Drawing.Point(87, 33)
+        Me.idBox.Name = "idBox"
+        Me.idBox.Size = New System.Drawing.Size(69, 19)
+        Me.idBox.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(38, 210)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(29, 12)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "特記"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(38, 124)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "勤務"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(38, 81)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 12)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "形態"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Blue
+        Me.Label1.Location = New System.Drawing.Point(38, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "職員No."
+        '
+        'dgvNamM
+        '
+        Me.dgvNamM.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvNamM.Location = New System.Drawing.Point(19, 267)
+        Me.dgvNamM.Name = "dgvNamM"
+        Me.dgvNamM.RowTemplate.Height = 21
+        Me.dgvNamM.Size = New System.Drawing.Size(775, 339)
+        Me.dgvNamM.TabIndex = 1
+        '
+        '職員マスタ
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "職員マスタ"
+        Me.ClientSize = New System.Drawing.Size(815, 627)
+        Me.Controls.Add(Me.dgvNamM)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "職員マスタ"
+        Me.Text = "Diary 職員マスタ"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.dgvNamM, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents idBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents namBox As System.Windows.Forms.TextBox
+    Friend WithEvents syuLabel As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnRegist As System.Windows.Forms.Button
+    Friend WithEvents memoBox As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents syuListBox As System.Windows.Forms.ListBox
+    Friend WithEvents RadioButton9 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton10 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton11 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton12 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton5 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton6 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton7 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton8 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents dgvNamM As System.Windows.Forms.DataGridView
 End Class
--- Symphony_Diary/Symphony_Diary/職員マスタ.vb
+++ Symphony_Diary/Symphony_Diary/職員マスタ.vb
@@ -1,3 +1,228 @@
-﻿Public Class 職員マスタ
+﻿Imports System.Data.OleDb
 
+Public Class 職員マスタ
+
+    ''' <summary>
+    ''' 行ヘッダーのカレントセルを表す三角マークを非表示に設定する為のクラス。
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class dgvRowHeaderCell
+
+        'DataGridViewRowHeaderCell を継承
+        Inherits DataGridViewRowHeaderCell
+
+        'DataGridViewHeaderCell.Paint をオーバーライドして行ヘッダーを描画
+        Protected Overrides Sub Paint(ByVal graphics As Graphics, ByVal clipBounds As Rectangle, _
+           ByVal cellBounds As Rectangle, ByVal rowIndex As Integer, ByVal cellState As DataGridViewElementStates, _
+           ByVal value As Object, ByVal formattedValue As Object, ByVal errorText As String, _
+           ByVal cellStyle As DataGridViewCellStyle, ByVal advancedBorderStyle As DataGridViewAdvancedBorderStyle, _
+           ByVal paintParts As DataGridViewPaintParts)
+            '標準セルの描画からセル内容の背景だけ除いた物を描画(-5)
+            MyBase.Paint(graphics, clipBounds, cellBounds, rowIndex, cellState, value, _
+                     formattedValue, errorText, cellStyle, advancedBorderStyle, _
+                     Not DataGridViewPaintParts.ContentBackground)
+        End Sub
+
+    End Class
+
+    ''' <summary>
+    ''' コンストラクタ
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub New()
+        InitializeComponent()
+        Me.StartPosition = FormStartPosition.CenterScreen
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+    End Sub
+
+    ''' <summary>
+    ''' loadイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub 職員マスタ_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        '職種リストボックス初期設定
+        initSyuListBox()
+
+        'データグリッドビュー初期設定
+        initDgvNamM()
+
+        'マスタデータ表示
+        displayNamM()
+    End Sub
+
+    ''' <summary>
+    ''' データグリッドビュー初期設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initDgvNamM()
+        Util.EnableDoubleBuffering(dgvNamM)
+
+        With dgvNamM
+            .AllowUserToAddRows = False '行追加禁止
+            .AllowUserToResizeColumns = False '列の幅をユーザーが変更できないようにする
+            .AllowUserToResizeRows = False '行の高さをユーザーが変更できないようにする
+            .AllowUserToDeleteRows = False '行削除禁止
+            .BorderStyle = BorderStyle.FixedSingle
+            .MultiSelect = False
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+            .DefaultCellStyle.BackColor = Color.FromKnownColor(KnownColor.Control)
+            .DefaultCellStyle.ForeColor = Color.Black
+            '.DefaultCellStyle.SelectionBackColor = Color.FromKnownColor(KnownColor.Control)
+            '.DefaultCellStyle.SelectionForeColor = Color.Black
+            .RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing
+            .ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+            .ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            .ColumnHeadersHeight = 17
+            .RowHeadersWidth = 28
+            .RowTemplate.Height = 16
+            .RowTemplate.HeaderCell = New dgvRowHeaderCell() '行ヘッダの三角マークを非表示に
+            .BackgroundColor = Color.FromKnownColor(KnownColor.Control)
+            .ShowCellToolTips = False
+            .EnableHeadersVisualStyles = False
+            .Font = New Font("ＭＳ Ｐゴシック", 8.5)
+            .ReadOnly = False
+        End With
+    End Sub
+
+    ''' <summary>
+    ''' マスタデータ表示
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub displayNamM()
+        '内容クリア
+        dgvNamM.Columns.Clear()
+
+        'データ取得
+        Dim cnn As New ADODB.Connection
+        cnn.Open(TopForm.DB_Diary)
+        Dim rs As New ADODB.Recordset
+        Dim sql As String = "select Id, Nam, Syu, Kei, Kin, Memo from NamM order by Id"
+        rs.Open(sql, cnn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockReadOnly)
+        Dim da As OleDbDataAdapter = New OleDbDataAdapter()
+        Dim ds As DataSet = New DataSet()
+        da.Fill(ds, rs, "NamM")
+        Dim dt As DataTable = ds.Tables("NamM")
+
+        '表示
+        dgvNamM.DataSource = dt
+        dgvNamM.ClearSelection()
+
+        '幅設定等
+        With dgvNamM
+            With .Columns("Id")
+                .HeaderText = "職員No."
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 60
+            End With
+            With .Columns("Nam")
+                .HeaderText = "氏名"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 100
+            End With
+            With .Columns("Syu")
+                .HeaderText = "職種"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 110
+            End With
+            With .Columns("Kei")
+                .HeaderText = "形態"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 93
+            End With
+            With .Columns("Kin")
+                .HeaderText = "勤務"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 115
+            End With
+            With .Columns("Memo")
+                .HeaderText = "特記"
+                .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft
+                .SortMode = DataGridViewColumnSortMode.NotSortable
+                .Width = 250
+            End With
+        End With
+
+    End Sub
+
+    ''' <summary>
+    ''' 内容クリア
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub clearText()
+        idBox.Text = ""
+        namBox.Text = ""
+        syuLabel.Text = ""
+        memoBox.Text = ""
+    End Sub
+
+    ''' <summary>
+    ''' 職種リストボックス初期設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initSyuListBox()
+        Dim syuArray() As String = {"理事長", "施設長", "副施設長", "事務局長", "部長", "課長", "係長", "主任", "管理者", "ｻｰﾋﾞｽ提供責任者", "医師", "正看護師", "准看護師", "看護職", "機能訓練士", "介護支援専門員", "生活相談員", "支援援助員", "介護職", "介護福祉士", "訪問介護員", "管理栄養士", "栄養士", "宿直"}
+        syuListBox.Items.Clear()
+        syuListBox.Items.AddRange(syuArray)
+    End Sub
+
+    ''' <summary>
+    ''' CellMouseClickイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub dgvNamM_CellMouseClick(sender As Object, e As System.Windows.Forms.DataGridViewCellMouseEventArgs) Handles dgvNamM.CellMouseClick
+        If e.RowIndex >= 0 Then
+            Dim id As Integer = dgvNamM("Id", e.RowIndex).Value
+            Dim nam As String = Util.checkDBNullValue(dgvNamM("Nam", e.RowIndex).Value)
+            Dim syu As String = Util.checkDBNullValue(dgvNamM("Syu", e.RowIndex).Value)
+            Dim kei As String = Util.checkDBNullValue(dgvNamM("Kei", e.RowIndex).Value)
+            Dim kin As String = Util.checkDBNullValue(dgvNamM("Kin", e.RowIndex).Value)
+            Dim memo As String = Util.checkDBNullValue(dgvNamM("Memo", e.RowIndex).Value)
+
+            clearText()
+
+            '画面に値反映
+            idBox.Text = id
+            namBox.Text = nam
+            syuLabel.Text = syu
+            memoBox.Text = memo
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' CellPaintingイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub dgvNamM_CellPainting(sender As Object, e As System.Windows.Forms.DataGridViewCellPaintingEventArgs) Handles dgvNamM.CellPainting
+        '行ヘッダーかどうか調べる
+        If e.ColumnIndex < 0 AndAlso e.RowIndex >= 0 Then
+            'セルを描画する
+            e.Paint(e.ClipBounds, DataGridViewPaintParts.All)
+
+            '行番号を描画する範囲を決定する
+            'e.AdvancedBorderStyleやe.CellStyle.Paddingは無視しています
+            Dim indexRect As Rectangle = e.CellBounds
+            indexRect.Inflate(-2, -2)
+            '行番号を描画する
+            TextRenderer.DrawText(e.Graphics, _
+                (e.RowIndex + 1).ToString(), _
+                e.CellStyle.Font, _
+                indexRect, _
+                e.CellStyle.ForeColor, _
+                TextFormatFlags.HorizontalCenter Or TextFormatFlags.VerticalCenter)
+            '描画が完了したことを知らせる
+            e.Handled = True
+        End If
+    End Sub
 End Class
