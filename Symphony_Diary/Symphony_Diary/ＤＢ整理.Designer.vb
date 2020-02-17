af6485d0854c4849e40658456091383b1e8e931a
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2020-02-17 13:56:11 +0900

    DB整理作成

--- Symphony_Diary/Symphony_Diary/ＤＢ整理.Designer.vb
+++ Symphony_Diary/Symphony_Diary/ＤＢ整理.Designer.vb
@@ -22,8 +22,65 @@ Partial Class ＤＢ整理
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.btnDBDelete = New System.Windows.Forms.Button()
+        Me.deleteProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnDBDelete
+        '
+        Me.btnDBDelete.Location = New System.Drawing.Point(299, 104)
+        Me.btnDBDelete.Name = "btnDBDelete"
+        Me.btnDBDelete.Size = New System.Drawing.Size(66, 27)
+        Me.btnDBDelete.TabIndex = 9
+        Me.btnDBDelete.Text = "実行"
+        Me.btnDBDelete.UseVisualStyleBackColor = True
+        '
+        'deleteProgressBar
+        '
+        Me.deleteProgressBar.Location = New System.Drawing.Point(154, 109)
+        Me.deleteProgressBar.Name = "deleteProgressBar"
+        Me.deleteProgressBar.Size = New System.Drawing.Size(119, 16)
+        Me.deleteProgressBar.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS UI Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.Location = New System.Drawing.Point(46, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 14)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "勤務表"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(23, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(347, 15)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "処理効率向上のため、次のデータの５年以前を整理します"
+        '
+        'ＤＢ整理
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(405, 155)
+        Me.Controls.Add(Me.btnDBDelete)
+        Me.Controls.Add(Me.deleteProgressBar)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "ＤＢ整理"
         Me.Text = "ＤＢ整理"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents btnDBDelete As System.Windows.Forms.Button
+    Friend WithEvents deleteProgressBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
--- Symphony_Diary/Symphony_Diary/ＤＢ整理.vb
+++ Symphony_Diary/Symphony_Diary/ＤＢ整理.vb
@@ -1,3 +1,43 @@
 ﻿Public Class ＤＢ整理
 
+    Public Sub New()
+        InitializeComponent()
+        Me.StartPosition = FormStartPosition.CenterScreen
+        Me.MinimizeBox = False
+        Me.MaximizeBox = False
+    End Sub
+
+    Private Sub ＤＢ整理_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    ''' <summary>
+    ''' 実行ボタンクリックイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub btnDBDelete_Click(sender As System.Object, e As System.EventArgs) Handles btnDBDelete.Click
+        deleteProgressBar.Minimum = 0
+        deleteProgressBar.Maximum = 100
+        deleteProgressBar.Value = 0
+
+        '現在年月
+        Dim nowYmStr As String = Today.ToString("yyyy/MM")
+        '５年前年月
+        Dim targetYmStr As String = Today.AddYears(-5).ToString("yyyy/MM")
+
+        Dim cnn As New ADODB.Connection
+        cnn.Open(TopForm.DB_Diary)
+
+        '勤務割データ削除
+        Dim cmd As New ADODB.Command()
+        cmd.ActiveConnection = cnn
+        cmd.CommandText = "delete from KinD where Ym < '" & targetYmStr & "'"
+        cmd.Execute()
+
+        deleteProgressBar.Value = 100
+
+        MsgBox("データを削除しました。" & Environment.NewLine & "単独モードでDBCompactを実行して下さい。", MsgBoxStyle.Information)
+    End Sub
 End Class
